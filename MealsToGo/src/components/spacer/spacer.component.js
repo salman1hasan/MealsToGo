171adf37ed28d359e87557cf7241904e22bf8566
Salman Hasan [Salman Zuberi Hussain] <96196317+salman1hasan@users.noreply.github.com>
2023-10-28 09:31:06 -0400

    Update spacer.component.js

--- MealsToGo/src/components/spacer/spacer.component.js
+++ MealsToGo/src/components/spacer/spacer.component.js
@@ -1,30 +1,31 @@
-import styled from "styled-components/native";
+import styled from 'styled-components/native';
 
- const sizeVariant = {
-   small: 1,
-   medium: 2,
-   large: 3,
- };
+const sizeVariant = {
+  small: 1,
+  medium: 2,
+  large: 3,
+};
 
- const positionVariant = {
-   top: "marginTop",
-   left: "marginLeft",
-   right: "marginRight",
-   bottom: "marginBottom",
- };
+const positionVariant = {
+  top: 'marginTop',
+  left: 'marginLeft',
+  right: 'marginRight',
+  bottom: 'marginBottom',
+};
 
- const getVariant = (position, size, theme) => {
-   const sizeIndex = sizeVariant[size];
-   const property = positionVariant[position];
-   const value = theme.space[sizeIndex];
+const getVariant = (position, size, theme) => {
+  const sizeIndex = sizeVariant[size];
+  const property = positionVariant[position];
+  const value = theme.space[sizeIndex];
 
-   return `${property}:${value}`;
- };
+  return `${property}:${value}`;
+};
 
- export const Spacer = styled.View`
-   ${({ position, size, theme }) => getVariant(position, size, theme)}
- `;
+export const Spacer = styled.View`
+  ${({ position, size, theme }) => getVariant(position, size, theme)}
+`;
 
- Spacer.defaultProps = {
-   position: "top",
-   size: "small",
+Spacer.defaultProps = {
+  position: 'top',
+  size: 'small',
+};
