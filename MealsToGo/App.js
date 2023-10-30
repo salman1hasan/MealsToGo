760fa93897bf4b64daf70bf8edaf2f12e17ac0de
Salman Hasan [Salman Zuberi Hussain] <96196317+salman1hasan@users.noreply.github.com>
2023-10-30 17:22:30 -0400

    Add files via upload

--- MealsToGo/App.js
+++ MealsToGo/App.js
@@ -1,35 +1,85 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StatusBar, StyleSheet, SafeAreaView, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign } from '@expo/vector-icons';
+
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from '@expo-google-fonts/oswald';
+import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
+
+import { theme } from './src/infrastructure/theme';
+import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
+import { SafeArea } from './src/components/safe-area.component';
+
+const Tab = createBottomTabNavigator();
+
+const Settings = () => (
+  <SafeArea>
+    <Text>Settings</Text>
+  </SafeArea>
+);
+const shoppingcart = () => (
+  <SafeArea>
+    <Text>Shopping Cart</Text>
+  </SafeArea>
+);
+
+const qrcode = () => (
+  <SafeArea>
+    <Text>qrcode</Text>
+  </SafeArea>
+);
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Text>search</Text>
-        </View>
-        <View style={styles.list}>
-          <Text>list</Text>
-        </View>
-      </SafeAreaView>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ color, size }) => {
+                let iconName;
+                if (route.name === 'Content') {
+                  iconName = 'videocamera';
+                } else if (route.name === 'Shopping Cart') {
+                  iconName = 'shoppingcart';
+                } else if (route.name === 'qrcode') {
+                  iconName = 'qrcode';
+                }
+
+                // You can return any component that you like here!
+                return <AntDesign name={iconName} size={size} color={color} />;
+              },
+            })}
+            tabBarOptions={{
+              activeTintColor: 'tomato',
+              inactiveTintColor: 'gray',
+            }}
+          >
+            <Tab.Screen name="Content" component={RestaurantsScreen} />
+            <Tab.Screen name="Shopping Cart" component={shoppingcart} />
+            <Tab.Screen name="qrcode" component={qrcode} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-    backgroundColor: 'green',
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: 'blue',
-  },
-});
