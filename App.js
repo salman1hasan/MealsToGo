9b32b1d345dfee2944bf9b981b9e8784cee139a3
Salman Hasan <s.hasan@unguarded.com>
2023-11-01 11:51:57 -0400

    Commit

---
+++ App.js
@@ -0,0 +1,86 @@
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign } from '@expo/vector-icons';
+
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from '@expo-google-fonts/oswald';
+import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
+
+import { theme } from './src/infrastructure/theme';
+import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
+import { SafeArea } from './src/components/utility/safe-area.component';
+import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context';
+import { LocationContextProvider } from './src/services/location/location.context';
+
+const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Store: 'shoppingcart',
+  Content: 'videocamera',
+  Qrcode: 'qrcode',
+};
+
+const qrcode = () => (
+  <SafeArea>
+    <Text>UNGUARDED QRCode</Text>
+  </SafeArea>
+);
+const Content = () => (
+  <SafeArea>
+    <Text>UNGUARDED Content</Text>
+  </SafeArea>
+);
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <AntDesign name={iconName} size={size} color={color} />
+    ),
+  };
+};
+
+export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <LocationContextProvider>
+          <RestaurantsContextProvider>
+            <NavigationContainer>
+              <Tab.Navigator
+                screenOptions={createScreenOptions}
+                tabBarOptions={{
+                  activeTintColor: 'tomato',
+                  inactiveTintColor: 'gray',
+                }}
+              >
+                <Tab.Screen name="Store" component={RestaurantsScreen} />
+                <Tab.Screen name="Content" component={Content} />
+                <Tab.Screen name="Qrcode" component={qrcode} />
+              </Tab.Navigator>
+            </NavigationContainer>
+          </RestaurantsContextProvider>
+        </LocationContextProvider>
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+}
---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import { Text, Image, View } from 'react-native';
+import { Card } from 'react-native-paper';
+import { SvgXml } from 'react-native-svg';
+
+import { Spacer } from '../../../components/spacer/spacer.component';
+import star from '../../../../assets/star';
+import open from '../../../../assets/open';
+
+const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const Title = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = '                           UNGUARDED Sticker',
+    icon = 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
+    photos = [
+      'file:///Users/salmanhasan/Downloads/Untitled%20design%20-%202023-10-28T150231.580.png',
+    ],
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily = true,
+  } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+        <Title>{name}</Title>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: 'black' }}>
+                OPEN
+              </Text>
+            )}
+            <Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            </Spacer>
+          </SectionEnd>
+        </Section>
+      </Info>
+    </RestaurantCard>
+  );
+};
